b503c57d024b669be504c1b31704b094e014ac28
Randy Wong <rtwong@ualberta.ca>
2016-12-05 15:06:33 -0700

    asdf

--- LevelUp/MainWindow.xaml.cs
+++ LevelUp/MainWindow.xaml.cs
@@ -219,36 +219,19 @@ namespace LevelUp
             skillProgressBar.Width = 270;
 
             Button timeAddButton = new Button();
-            timeAddButton.Click += new RoutedEventHandler(addHalfHour);
-            Console.Write(skillToAdd.name);
-            timeAddButton.Name = skillToAdd.identifier;
             timeAddButton.Height = 48;
             timeAddButton.Width = 55;
             timeAddButton.Content = "+";
 
-            skillPanel.Children.Add(skillPic);
-            skillPanel.Children.Add(levelLabel);
-            skillPanel.Children.Add(skillLabel);
-            skillPanel.Children.Add(skillProgressBar);
-            skillPanel.Children.Add(timeAddButton);
+           skillPanel.Children.Add(skillPic);
+           skillPanel.Children.Add(levelLabel);
+           skillPanel.Children.Add(skillLabel);
+           skillPanel.Children.Add(skillProgressBar);
+           skillPanel.Children.Add(timeAddButton);
 
 
-            skillContainer.Children.Add(skillPanel);
+           skillContainer.Children.Add(skillPanel);
 
-        }
-
-        public void addHalfHour(object sender, RoutedEventArgs e)
-        {
-
-            Button clickedButton = (Button)sender;
-            foreach (Skill entry in dataManager.data)
-            {
-                if (clickedButton.Name == entry.identifier)
-                {
-                    dataManager.update_data(entry.identifier, 0.5);
-                }   
-            }
-            populate();
         }
 
 
