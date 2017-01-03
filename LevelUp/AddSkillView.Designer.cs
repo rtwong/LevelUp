720091f8c632d9e4d94f904da15bd2f4dd9038df
Marcin <pietrasi@ualberta.ca>
2017-01-03 13:40:53 -0700

    addSkillTitleBar

--- LevelUp/AddSkillView.Designer.cs
+++ LevelUp/AddSkillView.Designer.cs
@@ -1,11 +1,15 @@
-﻿namespace LevelUp
+﻿using System.Drawing;
+
+namespace LevelUp
 {
+
     partial class AddSkillView
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
 
         /// <summary>
         /// Clean up any resources being used.
@@ -35,6 +39,11 @@
             this.creativeCheckbox = new System.Windows.Forms.CheckBox();
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
+            this.titleBarFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.titleBarDragLabel = new System.Windows.Forms.Label();
+            this.titleBarClose = new System.Windows.Forms.PictureBox();
+            this.titleBarFlowLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBarClose)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,7 +59,7 @@
             // 
             this.skillTextBox.Location = new System.Drawing.Point(16, 55);
             this.skillTextBox.Name = "skillTextBox";
-            this.skillTextBox.Size = new System.Drawing.Size(414, 20);
+            this.skillTextBox.Size = new System.Drawing.Size(271, 20);
             this.skillTextBox.TabIndex = 1;
             // 
             // intellectCheckbox
@@ -67,7 +76,7 @@
             // strengthCheckbox
             // 
             this.strengthCheckbox.AutoSize = true;
-            this.strengthCheckbox.Location = new System.Drawing.Point(185, 118);
+            this.strengthCheckbox.Location = new System.Drawing.Point(106, 118);
             this.strengthCheckbox.Name = "strengthCheckbox";
             this.strengthCheckbox.Size = new System.Drawing.Size(86, 17);
             this.strengthCheckbox.TabIndex = 3;
@@ -78,7 +87,7 @@
             // creativeCheckbox
             // 
             this.creativeCheckbox.AutoSize = true;
-            this.creativeCheckbox.Location = new System.Drawing.Point(348, 118);
+            this.creativeCheckbox.Location = new System.Drawing.Point(198, 118);
             this.creativeCheckbox.Name = "creativeCheckbox";
             this.creativeCheckbox.Size = new System.Drawing.Size(79, 17);
             this.creativeCheckbox.TabIndex = 4;
@@ -88,7 +97,7 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(361, 223);
+            this.cancelButton.Location = new System.Drawing.Point(167, 223);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 5;
@@ -98,7 +107,7 @@
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(269, 223);
+            this.okButton.Location = new System.Drawing.Point(86, 223);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 6;
@@ -106,11 +115,43 @@
             this.okButton.UseVisualStyleBackColor = true;
             this.okButton.Click += new System.EventHandler(this.okClick);
             // 
+            // titleBarFlowLayoutPanel
+            // 
+            this.titleBarFlowLayoutPanel.Controls.Add(this.titleBarDragLabel);
+            this.titleBarFlowLayoutPanel.Controls.Add(this.titleBarClose);
+            this.titleBarFlowLayoutPanel.Location = new System.Drawing.Point(2, 3);
+            this.titleBarFlowLayoutPanel.Name = "titleBarFlowLayoutPanel";
+            this.titleBarFlowLayoutPanel.Size = new System.Drawing.Size(298, 46);
+            this.titleBarFlowLayoutPanel.TabIndex = 7;
+            // 
+            // titleBarDragLabel
+            // 
+            this.titleBarDragLabel.Location = new System.Drawing.Point(3, 0);
+            this.titleBarDragLabel.Name = "titleBarDragLabel";
+            this.titleBarDragLabel.Size = new System.Drawing.Size(253, 24);
+            this.titleBarDragLabel.TabIndex = 0;
+            this.titleBarDragLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titleBarDragLabel_MouseDown);
+            // 
+            // titleBarClose
+            // 
+            this.titleBarClose.BackgroundImage = global::LevelUp.Properties.Resources.close_Icon;
+            this.titleBarClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.titleBarClose.InitialImage = global::LevelUp.Properties.Resources.close_Icon;
+            this.titleBarClose.Location = new System.Drawing.Point(259, 0);
+            this.titleBarClose.Margin = new System.Windows.Forms.Padding(0);
+            this.titleBarClose.Name = "titleBarClose";
+            this.titleBarClose.Size = new System.Drawing.Size(39, 24);
+            this.titleBarClose.TabIndex = 2;
+            this.titleBarClose.TabStop = false;
+            this.titleBarClose.Click += new System.EventHandler(this.titleBarClose_Click);
+            // 
             // AddSkillView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 258);
+            this.ClientSize = new System.Drawing.Size(299, 372);
+            this.ControlBox = false;
+            this.Controls.Add(this.titleBarFlowLayoutPanel);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.creativeCheckbox);
@@ -118,8 +159,11 @@
             this.Controls.Add(this.intellectCheckbox);
             this.Controls.Add(this.skillTextBox);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddSkillView";
             this.Text = "Add Skill";
+            this.titleBarFlowLayoutPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.titleBarClose)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,5 +178,8 @@
         private System.Windows.Forms.CheckBox creativeCheckbox;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.FlowLayoutPanel titleBarFlowLayoutPanel;
+        private System.Windows.Forms.Label titleBarDragLabel;
+        private System.Windows.Forms.PictureBox titleBarClose;
     }
 }
--- LevelUp/AddSkillView.cs
+++ LevelUp/AddSkillView.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +17,16 @@ namespace LevelUp
         public bool creativeValue { get; set; }
         public bool strengthValue { get; set; }
         public bool intellectValue { get; set; }
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
         public AddSkillView()
         {
@@ -67,5 +78,23 @@ namespace LevelUp
             }
         }
 
+        private void titleBarClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+
+            this.Close();
+        }
+
+        private void titleBarDragLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("qweqwe");
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+            
+        }
     }
 }
